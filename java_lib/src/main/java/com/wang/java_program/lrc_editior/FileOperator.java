e8a1c81dd114648e54a67391b4552223f9928ca9
wangrongjun <15521302230@163.com>
2017-05-22 13:13:40 +0800

    java_lib: 1.LrcWindow：酷狗krc双译歌词转lrc小程序 2.ClipboardUtil：添加剪切板变化监听功能（500ms一次的主动监听）

--- java_lib/src/main/java/com/wang/java_program/lrc_editior/FileOperator.java
+++ java_lib/src/main/java/com/wang/java_program/lrc_editior/FileOperator.java
@@ -11,52 +11,52 @@ import java.io.OutputStreamWriter;
 
 public class FileOperator {
 
-	/**
-	 * 注意：使用bufferedReader.readLine得到的每一句话会在后面人为地加上 "\r\n"
-	 */
-	public static String readFile(File file, String charsetName)
-			throws Exception {
-		try {
-			FileInputStream fis = new FileInputStream(file);
-			InputStreamReader isr = new InputStreamReader(fis, charsetName);
-			BufferedReader br = new BufferedReader(isr);
-			String line = "";
-			StringBuilder builder = new StringBuilder();
+    /**
+     * 注意：使用bufferedReader.readLine得到的每一句话会在后面人为地加上 "\r\n"
+     */
+    public static String readFile(File file, String charsetName)
+            throws Exception {
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            InputStreamReader isr = new InputStreamReader(fis, charsetName);
+            BufferedReader br = new BufferedReader(isr);
+            String line = "";
+            StringBuilder builder = new StringBuilder();
 
-			while ((line = br.readLine()) != null) {
-				builder.append(line + "\r\n");
-			}
+            while ((line = br.readLine()) != null) {
+                builder.append(line + "\r\n");
+            }
 
-			br.close();
-			isr.close();
-			fis.close();
+            br.close();
+            isr.close();
+            fis.close();
 
-			return builder.toString();
+            return builder.toString();
 
-		} catch (IOException e) {
-			throw new Exception(e.getMessage());
-		}
-	}
+        } catch (IOException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
 
-	public static void saveFile(File file, String charsetName, String text)
-			throws Exception {
-		try {
-			if (!file.exists()) {
-				file.createNewFile();
-			}
+    public static void saveFile(File file, String charsetName, String text)
+            throws Exception {
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
 
-			FileOutputStream fos = new FileOutputStream(file);
-			OutputStreamWriter osr = new OutputStreamWriter(fos, charsetName);
-			BufferedWriter bw = new BufferedWriter(osr);
-			bw.write(text);
+            FileOutputStream fos = new FileOutputStream(file);
+            OutputStreamWriter osr = new OutputStreamWriter(fos, charsetName);
+            BufferedWriter bw = new BufferedWriter(osr);
+            bw.write(text);
 
-			bw.close();
-			osr.close();
-			fos.close();
+            bw.close();
+            osr.close();
+            fos.close();
 
-		} catch (Exception e) {
-			throw new Exception(e.getMessage());
-		}
-	}
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
 
 }
